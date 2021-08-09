d1e705604ad7ce5f53a93a7135fa479fcb1acd0f
elodiebhs <elodiebouthors@hotmail.com>
2021-08-08 19:13:18 -0700

    set up story for button in storybook

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Button} from '../components/Button';
+export default{component : Button, title : 'components/Button'}
+
+export const Primary = () => <Button primary>Button</Button>;
